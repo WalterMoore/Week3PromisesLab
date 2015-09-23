e524d069e562176ea561d3eaf9af797bec93e6a9
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-09-23 10:04:01 -0500

    morning work

--- Templates/UsePromises/usePromiseTemplate.js
+++ Templates/UsePromises/usePromiseTemplate.js
@@ -1,3 +1,36 @@
-function mapAsync(iterator, obj, context) {};
-function mapAsyncInOrder(iterator, array, context) {};
-function mapAsyncInDescendingOrder(iterator, array, context) {};
+function mapAsync(iterator, obj, context) {
+	var arr = [];
+	var n = obj.length;
+	for (var i = 0; i < n; i++){
+		var collect = iterator.call(context,obj[i],i,obj);
+		arr.push(collect);
+	}
+	return arr;
+};
+//------------------------
+		/*map : function(list, iterator) {
+			var arr = [];
+			var n = list.length;
+			for (var i = 0; i < n; i++){
+            iterator(list[i]);
+			arr.push(iterator(list[i]));
+			//--the below also works instead of the one line above:
+			//var temp = iterator(list[i]);
+			//	arr.push(temp);
+			}
+			return arr;
+
+		},*/
+//------------------------
+
+
+
+function mapAsyncInOrder(iterator, array, context) {
+	
+};
+
+
+
+function mapAsyncInDescendingOrder(iterator, array, context) {
+	
+};
