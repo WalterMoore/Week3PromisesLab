61f13dd5bae690fe0f5fac77f6982c23b36bfbb9
bamaswampmonster <bamaswampmonster@bamaswastersmbp.attlocal.net>
2015-09-23 20:49:13 -0500

    ganz nichts

--- Templates/CreatePromises/createPromiseTemplate.js
+++ Templates/CreatePromises/createPromiseTemplate.js
@@ -1,11 +1,25 @@
 var APromise = {};
 
-APromise.all = function(promises){};
+APromise.all = function(promises){
+	return APromise;
+};
 
-APromise.race = function(promises){};
+
+APromise.race = function(promises){
+	
+};
+
+
+
 
 APromise.resolve = function(value){};
 
+
+
+
 APromise.reject = function(err){};
+
+
+
 
 module.exports.APromise = APromise;
